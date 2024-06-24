bc901a28c7fc5cbc539097bf222b3e1c4eaea33c
boytrend0108 <boytrend@gmail.com>
2024-06-24 19:12:23 +0300

    make build

---
+++ index.dada1974.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".page__content");window.addEventListener("scroll",function(){document.documentElement.scrollTop>.7*document.documentElement.scrollHeight?e.style.pointerEvents="none":e.style.pointerEvents="all"});
+//# sourceMappingURL=index.dada1974.js.map
